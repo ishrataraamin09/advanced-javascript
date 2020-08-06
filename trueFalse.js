43714ba10fff54ec99b3c2819d382c1539848136
ishrataraamin09 <67515063+ishrataraamin09@users.noreply.github.com>
2020-08-07 01:42:17 +0600

    true vs flase

---
+++ trueFalse.js
@@ -0,0 +1,13 @@
+//Falsey { 0 , "" , undefined , null , NaN , name = false}
+//truthy { " " , "0" , [] , {} , 'false' }
+
+//const age =4 ;
+//const name = "" ;
+let name = null ;
+console.log(name) ;
+
+if (name > 0){
+    console.log("Condition is true") ;
+} else {
+    console.log("Condition is false") ;
+}
