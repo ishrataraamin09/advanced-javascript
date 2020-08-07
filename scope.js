ea1abb53637c292888b92974442d0c350fc02345
ishrataraamin09 <67515063+ishrataraamin09@users.noreply.github.com>
2020-08-07 17:36:48 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,19 @@
+let bonus = 20 ;
+
+function sum (first , second) {
+    let result = first + second + bonus ;
+    //console.log(result) ;
+    if(result > 9 ){
+        let mood =  "happy" ;
+        mood =  "fishy" ;
+        mood =  "moody" ;
+        mood =  "cranky" ;
+        console.log(mood) ;
+    }
+    return result ;
+    
+    
+}
+const output = sum( 9, 5) ;
+//console.log(output) ;
+//console.log(bonus) ;
