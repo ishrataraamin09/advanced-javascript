f10c5a64fd084113dfe46baa27640df151142345
ishrataraamin09 <67515063+ishrataraamin09@users.noreply.github.com>
2020-08-07 15:34:45 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+let bird;
+console.log(bird) ;
+
+function add(num1 , num2){
+    console.log(num1 + num2) ;
+    return ;
+}
+const result = add(13 , 82) ;
+console.log(result) ;
+
+function add(num1 , num2){
+    console.log(num1 , num2) ;
+}
+const result = add(13) ;
+console.log(result) ;
+
+const bFriend = {name: 'Mirak' , phone: 12344 , } ;
+console.log(bFriend.address) ;
+
+let fun = undefined ;
+console.log(fun) ;
+
+let ages = [3 , 7 , 9] ;
+console.log(ages[11]) ;
+
